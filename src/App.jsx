fa2b18ddbaa267f1ba65763c6c5174e11ebabe03
nodirdev984 <negmatovn984@gmail.com>
2024-02-08 21:01:26 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,83 @@
 import { useEffect, useState } from "react";
 
-const images = [
-  {
-    id: 1,
-    img: "./src/public/img.1.jpg",
-    isIt: false,
-  },
-  {
-    id: 2,
-    img: "./src/public/img2.jpg",
-    isIt: true,
-  },
-  {
-    id: 3,
-    img: "./src/public/img3.jpg",
-    isIt: false,
-  },
-  {
-    id: 4,
-    img: "./src/public/img4.jpg",
-    isIt: false,
-  },
-  {
-    id: 5,
-    img: "./src/public/img5.jpg",
-    isIt: false,
-  },
-  {
-    id: 6,
-    img: "./src/public/img6.jpg",
-    isIt: false,
-  },
-  {
-    id: 7,
-    img: "./src/public/img7.jpg",
-    isIt: false,
-  },
-  {
-    id: 8,
-    img: "./src/public/img8.jpg",
-    isIt: false,
-  },
-  {
-    id: 9,
-    img: "./src/public/img9.jpg",
-    isIt: false,
-  },
-  {
-    id: 10,
-    img: "./src/public/img10.jpg",
-    isIt: false,
-  },
-  {
-    id: 11,
-    img: "./src/public/img11.jpg",
-    isIt: false,
-  },
-  {
-    id: 12,
-    img: "./src/public/img12.jpg",
-    isIt: false,
-  },
-  {
-    id: 13,
-    img: "./src/public/img13.jpg",
-    isIt: false,
-  },
-  {
-    id: 14,
-    img: "./src/public/img14.jpg",
-    isIt: false,
-  },
-  {
-    id: 15,
-    img: "./src/public/img.15.jpg",
-    isIt: false,
-  },
-];
-
 function App() {
+  const images = [
+    {
+      id: 1,
+      img: "./src/public/img.1.jpg",
+      isIt: false,
+    },
+    {
+      id: 2,
+      img: "./src/public/img2.jpg",
+      isIt: true,
+    },
+    {
+      id: 3,
+      img: "./src/public/img3.jpg",
+      isIt: false,
+    },
+    {
+      id: 4,
+      img: "./src/public/img4.jpg",
+      isIt: false,
+    },
+    {
+      id: 5,
+      img: "./src/public/img5.jpg",
+      isIt: false,
+    },
+    {
+      id: 6,
+      img: "./src/public/img6.jpg",
+      isIt: false,
+    },
+    {
+      id: 7,
+      img: "./src/public/img7.jpg",
+      isIt: false,
+    },
+    {
+      id: 8,
+      img: "./src/public/img8.jpg",
+      isIt: false,
+    },
+    {
+      id: 9,
+      img: "./src/public/img9.jpg",
+      isIt: false,
+    },
+    {
+      id: 10,
+      img: "./src/public/img10.jpg",
+      isIt: false,
+    },
+    {
+      id: 11,
+      img: "./src/public/img11.jpg",
+      isIt: false,
+    },
+    {
+      id: 12,
+      img: "./src/public/img12.jpg",
+      isIt: false,
+    },
+    {
+      id: 13,
+      img: "./src/public/img13.jpg",
+      isIt: false,
+    },
+    {
+      id: 14,
+      img: "./src/public/img14.jpg",
+      isIt: false,
+    },
+    {
+      id: 15,
+      img: "./src/public/img.15.jpg",
+      isIt: false,
+    },
+  ];
   const [img, setImg] = useState();
   const hanleClick = (imgg) => {
     const oneIMg = images.find((imggg) => imggg.img === imgg.img);
